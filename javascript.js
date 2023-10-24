2faed97aaaa5fde43b909e5340b41ac7eed8d729
Code Boy <93109085+Codeboy16@users.noreply.github.com>
2023-10-24 10:42:19 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,9 @@
+document.getElementById('btn_buy_phone').addEventListener('click',()=>{
+    let audio = new Audio('buy_phone.mp3');
+    audio.play();
+})
+document.getElementById('btn_buy_phone1').addEventListener('click',()=>{
+    let audio = new Audio('buy_phone.mp3');
+    audio.play();
+})
+
